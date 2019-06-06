79f9386404345d289764be0b8335417a9540a7f1
Claudia Solbes <cssolbes@hotmail.com>
2019-06-06 00:22:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,2 @@
-speak_to_grandma == speak_to_grandma.upcase
-
-if speak_to_grandma == "Hi Nana, how are you?"
-  print "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma == "Hi"
-  print "HUH?! SPEAK UP, SONNY!"
-elsif speak_to_grandma == "WHAT DID YOU EAT TODAY?"
-  print "NO, NOT SINCE 1938!"
-elsif speak_to_grandma == "WHAT?"
-  print "NO, NOT SINCE 1938!"
-else speak_to_grandma == "I LOVE YOU GRANDMA!"
-  print "I LOVE YOU TOO PUMPKIN!"
-end
+def speak_to_grandma(phrase)
+  
