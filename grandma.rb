f384fe13721e6b0da79592b227fcb9a904ca630c
Claudia Solbes <cssolbes@hotmail.com>
2019-06-06 00:15:40 +0000

    Automatically backed up by Learn

---
+++ grandma.rb
@@ -0,0 +1,13 @@
+speak_to_grandma == phrase
+
+if phrase == "Hi Nana, how are you?"
+  print "HUH?! SPEAK UP, SONNY!"
+elsif phrase == "Hi"
+  print "HUH?! SPEAK UP, SONNY!"
+elsif phrase == "WHAT DID YOU EAT TODAY?"
+  print "NO, NOT SINCE 1938!"
+elsif phrase == "WHAT?"
+  print "NO, NOT SINCE 1938!"
+else phrase == "I LOVE YOU GRANDMA!"
+  print "I LOVE YOU TOO PUMPKIN!"
+end
